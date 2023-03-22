370581d3cc4cf8aaaf3444d0a8e320e24d268de9
Hangyeom Lee <h38lee@uwaterloo.ca>
2023-03-22 17:30:55 -0400

    V2 resolve the error from launching react

--- src/App.js
+++ src/App.js
@@ -1,70 +1,125 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 
-
-  
-function Article(probs){
+function Article(props){
   return <article>
-    <h2>{probs.title}</h2>
-    {probs.body}
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
 }
-
-function Header(probs){
-  console.log('probs : ', probs.title);
+function Header(props){
   return <header>
-    <h1><a href = "/" onClick={(event)=>{
+    <h1><a href="/" onClick={(event)=>{
       event.preventDefault();
-      probs.onChangeMode();
-    }}>{probs.title}</a></h1>
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
-
-
-function Nav(probs){
-  
+function Nav(props){
   const lis = []
-  for(let i = 0 ; i<probs.topics.length;i++){
-    let t = probs.topics[i];
-    lis.push(<li key={t.id}><a id = {t.id} href ={"/read/"+t.id} onClick = {(event)=>{
-      event.preventDefault();
-      probs.onChangeMode(event.target.id);
-    }}>{t.title}</a></li>)
-  }return <nav>
-  <ol>
-    {lis}
-  </ol>
-</nav>
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/'+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(Number(event.target.id));
+      }}>{t.title}</a>
+    </li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
 }
 
-  
 function App() {
-  const _mode = useState('WELCOME');
-  const mode = _mode[0];
-  const setMode = _mode[1];
-  console.log('_mode',_mode);
-  const topics = [
-    {id: 1, title: 'html', body : 'HTML is ...'},
-    {id: 2, title: 'css', body : 'CSS is ...'},
-    {id: 3, title: 'js', body : 'JAVA-script is ...'}
-  ]
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body:'css is ...'},
+    {id:3, title:'javascript', body:'javascript is ...'}
+  ]);
   let content = null;
-  if(mode ==='WELCOME'){
-    content = <Article title = "Welcome" body = "Hello WEB"></Article>
-  }
-  else if(mode === 'READ'){
-    content = <Article title = "Read" body = "Hello, Read"></Article>
+  let contextControl = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+    contextControl = <>
+      <li><a href={'/update/'+id} onClick={event=>{
+        event.preventDefault();
+        setMode('UPDATE');
+      }}>Update</a></li>
+      <li><input type="button" value="Delete" onClick={()=>{
+        const newTopics = []
+        for(let i=0; i<topics.length; i++){
+          if(topics[i].id !== id){
+            newTopics.push(topics[i]);
+          }
+        }
+        setTopics(newTopics);
+        setMode('WELCOME');
+      }} /></li>
+    </>
+  } else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
+  } else if(mode === 'UPDATE'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      console.log(title, body);
+      const newTopics = [...topics]
+      const updatedTopic = {id:id, title:title, body:body}
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('READ');
+    }}></Update>
   }
   return (
     <div>
-      <Header title = "REACT" onChangeMode={()=>{
-        mode = 'WELCOME';
+      <Header title="WEB" onChangeMode={()=>{
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics = {topics}onChangeMode={(id)=>{
-        mode = "READ";
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title = "Welcome" body = "Hello WEB"></Article>
+      {content}
+      <ul>
+        <li><a href="/create" onClick={event=>{
+          event.preventDefault();
+          setMode('CREATE');
+        }}>Create</a></li>
+        {contextControl}
+      </ul>
     </div>
   );
 }
